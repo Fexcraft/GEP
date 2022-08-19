c8a8182277df6944f212f62873058cd000dace25
Ferdinand Calo <fercalo96@yahoo.de>
2022-08-19 19:06:35 +0200

    Update GeneralEssentialsPack.java

--- src/main/java/net/fexcraft/mod/addon/gep/GeneralEssentialsPack.java
+++ src/main/java/net/fexcraft/mod/addon/gep/GeneralEssentialsPack.java
@@ -5,7 +5,7 @@ import net.fexcraft.mod.fvtm.data.addon.AddonClass;
 import net.minecraftforge.fml.common.Mod;
 
 @AddonClass(registryname = "fvtm:gep")
-@Mod(modid = "gep", name = "General Essentials Pack", version = "2.5.0", useMetadata = true, dependencies = "required-after:fcl;required-after:fvtm")
+@Mod(modid = "gep", name = "General Essentials Pack", version = "2.5.1", useMetadata = true, dependencies = "required-after:fcl;required-after:fvtm")
 public class GeneralEssentialsPack {
 	
 	public GeneralEssentialsPack(){
