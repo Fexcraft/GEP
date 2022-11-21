64d2f41bab0f12985a789162a329895b1f99ee12
Ferdinand Calo <fercalo96@yahoo.de>
2022-11-21 21:06:00 +0100

    Update SmelteryScript.java

--- src/main/java/net/fexcraft/mod/addon/gep/scripts/SmelteryScript.java
+++ src/main/java/net/fexcraft/mod/addon/gep/scripts/SmelteryScript.java
@@ -9,6 +9,8 @@ import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.mod.fvtm.block.generated.MultiblockTickableTE;
 import net.fexcraft.mod.fvtm.data.block.MB_Trigger;
 import net.fexcraft.mod.fvtm.data.block.MultiBlockData;
+import net.fexcraft.mod.fvtm.data.inv.InvHandlerFluid;
+import net.fexcraft.mod.fvtm.data.inv.InvHandlerVar;
 import net.fexcraft.mod.fvtm.util.script.DefaultCraftBlockScript;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
@@ -22,8 +24,6 @@ public class SmelteryScript extends DefaultCraftBlockScript {
 	
 	private static int heatincr;
 	private static float lavacon;
-	private int heat;
-	private int lava;//just copying tank status, for the gui
 	private boolean open;
 
 	public SmelteryScript(JsonObject obj){
@@ -35,41 +35,40 @@ public class SmelteryScript extends DefaultCraftBlockScript {
 	@Override
 	public void read(MultiBlockData data, NBTTagCompound tag){
 		super.read(data, tag);
-		heat = tag.hasKey("smeltery_heat") ? tag.getInteger("smeltery_heat") : 0;
 		open = tag.hasKey("smeltery_open") ? tag.getBoolean("smeltery_open") : false;
 	}
 
 	@Override
 	public NBTTagCompound write(MultiBlockData data, NBTTagCompound compound){
 		super.write(data, compound);
-		compound.setInteger("smeltery_heat", heat);
 		compound.setBoolean("smeltery_open", open);
 		return compound;
 	}
 	
 	@Override
 	public boolean ready(MultiblockTickableTE tile){
-		lava = tile.getMultiBlockData().getInventory("tank").getTank().getFluidAmount();
-		return heat > 1500;
+		return tile.getMultiBlockData().getInventory("heat").getVarValue() > 1500;
 	}
 	
 	@Override
 	public void prepare(MultiblockTickableTE tile){
-		if(lava <= 0){
+		InvHandlerFluid lava = (InvHandlerFluid)tile.getMultiBlockData().getInventory("tank");
+		InvHandlerVar heat = (InvHandlerVar)tile.getMultiBlockData().getInventory("heat");
+		if(lava.getTank().getFluidAmount() <= 0){
 			if(processed > 0) processed--;
-			if(heat > 0) heat--; 
+			if(heat.value() > 0) heat.shrink(); 
 			return;
 		}
 		int heatby = open ? heatincr * 2 : heatincr;
 		int lavat = (int)(heatby * lavacon);
 		if(lavat < 1) lavat = 1;
-		if(lava < lavat){
-			tile.getMultiBlockData().getInventory("tank").getTank().drain(lava, true);
-			heat += heatby > 2 ? heatby / 2 : 1;
+		if(lava.getTank().getFluidAmount() < lavat){
+			lava.getTank().drain(lava.getTank().getFluidAmount(), true);
+			heat.grow(heatby > 2 ? heatby / 2 : 1);
 			return;
 		}
 		tile.getMultiBlockData().getInventory("tank").getTank().drain(lavat, true);
-		heat += heatby;
+		heat.grow(heatby);
 	}
 	
 	@Override
@@ -77,7 +76,7 @@ public class SmelteryScript extends DefaultCraftBlockScript {
 		int coolby = open ? 2 : 1;
 		if(isCoolingDown()) coolby /= 2;
 		if(coolby <= 0) return;
-		heat -= coolby;
+		((InvHandlerVar)tile.getMultiBlockData().getInventory("heat")).shrink(coolby);
 	}
 
 	@Override
@@ -115,42 +114,22 @@ public class SmelteryScript extends DefaultCraftBlockScript {
 		return open && cooldown_speed > 1 ? cooldown_speed / 2 : cooldown_speed;
 	}
 	
-	public boolean consume(String value, int amount, boolean simulate){
+	@Override
+	public boolean consume(MultiBlockData data, String value, int amount, boolean simulate){
 		if(!value.equals("heat")) return true;
+		InvHandlerVar heat = (InvHandlerVar)data.getInventory("heat");
 		if(simulate){
-			return heat >= amount;
+			return heat.value() >= amount;
 		}
-		heat -= amount;
+		heat.shrink(amount);
 		return true;
-	}
-	
-	@Override
-	public int getConsumable(String id){
-		if(id.equals("heat")) return heat;
-		if(id.equals("lava")) return lava;
-		return 0;
-	}
-
-	@Override
-	public String[] getConsumables(){
-		return new String[]{ "heat", "lava" };
-	}
-
-	@Override
-	public void setConsumable(String id, int value){
-		if(id.equals("heat")){
-			heat = value;
-		}
-		if(id.equals("lava")){
-			lava = value;
-		}
 	}
 
 	@Override
 	public List<Object[]> getGuiElements(){
 		ArrayList<Object[]> list = new ArrayList<>();
 		list.add(new Object[]{ GuiElement.PROGRESS_BAR, "Heat/Temp", "heat", 2000, RGB.RED });
-		list.add(new Object[]{ GuiElement.TEXT_VALUE, "Lava Tank: %smB", "lava" });
+		list.add(new Object[]{ GuiElement.TEXT_VALUE, "Lava Tank: %smB", "tank" });
 		return list;
 	}
 
